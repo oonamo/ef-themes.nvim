174415d5c24a769a9641803cea7d4a9684cd8958
oonamo <onam75hernandez@gmail.com>
2025-01-26 20:47:27 -0800

    ci(extras): Fix regression where setup is needed for building

--- lua/ef-themes/themes/init.lua
+++ lua/ef-themes/themes/init.lua
@@ -1,10 +1,5 @@
 local M = {}
 
-local test_value = function(value)
-  local ok, err = pcall(vim.api.nvim_set_hl, 0, "dummy_test", { bg = value })
-  return ok
-end
-
 function M._resolve_palette(palette)
   for k, v in pairs(palette) do
     if v:sub(1, 1) ~= "#" then
--- tests/mininit.lua
+++ tests/mininit.lua
@@ -2,3 +2,4 @@ local bufname = vim.api.nvim_buf_get_name(0)
 local ef_theme_plugin = vim.fn.fnamemodify(bufname, ":h:h")
 
 vim.opt.rtp:append(ef_theme_plugin)
+require("ef-themes").setup()
