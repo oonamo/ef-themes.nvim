e979a51fabbfed67efb803b6b3fb47632210cc0f
oonamo <onam75hernandez@gmail.com>
2025-01-28 14:25:14 -0800

    feat(cmp): add cmp highlights

--- lua/ef-themes/groups/init.lua
+++ lua/ef-themes/groups/init.lua
@@ -2,6 +2,7 @@ local M = {
   groups = {
     "base",
     "blink",
+    "cmp",
     "fzf",
     "mini",
     "render_markdown",
@@ -43,13 +44,14 @@ function M.build(palette, opts, name, theme_opts)
   for modname, use in pairs(opts.modules or {}) do
     if use then
       if not vim.tbl_contains(M.groups, modname) then
-        return vim.notify(string.format("[ef-themes]: Module '%s' does not exist.", vim.log.levels.ERROR))
-      end
-      local mod_highlights = require("ef-themes.groups." .. modname).get(palette, opts)
-      for k, v in pairs(mod_highlights or {}) do
-        local hl = resolve_hl(v)
-        vim.api.nvim_set_hl(0, k, hl)
-        all_groups[k] = hl
+        vim.notify(string.format("[ef-themes]: Module '%s' does not exist.", vim.log.levels.ERROR))
+      else
+        local mod_highlights = require("ef-themes.groups." .. modname).get(palette, opts)
+        for k, v in pairs(mod_highlights or {}) do
+          local hl = resolve_hl(v)
+          vim.api.nvim_set_hl(0, k, hl)
+          all_groups[k] = hl
+        end
       end
     end
   end
