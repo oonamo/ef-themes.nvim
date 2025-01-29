3553cb335f848d30d9e89839994cf5bd83c2f272
oonamo <onam75hernandez@gmail.com>
2025-01-29 15:09:51 -0800

    refactor(mini): Improve MiniCursorword and MiniIcons highlighting and

--- lua/ef-themes/groups/mini.lua
+++ lua/ef-themes/groups/mini.lua
@@ -3,24 +3,25 @@ local M = {}
 ---@param c Ef-Theme
 ---@param opts Ef-themes.Config
 function M.get(c, opts)
+  local is_dark = require("ef-themes").is_dark(c)
   -- stylua: ignore
   return {
     MiniAnimateCursor             = { reverse   = true, nocombine          = true },
     MiniAnimateNormalFloat        = { link      = "NormalFloat" },
 
     MiniClueBorder                = { link      = "FloatBorder" },
-    MiniClueDescGroup             = { fg = c.keyword },
-    MiniClueDescSingle            = { fg = c.fg_main },
-    MiniClueNextKey               = { fg = c.keybind, bold = true },
-    MiniClueNextKeyWithPostkeys   = { fg = c.err },
-    MiniClueSeparator             = { fg = c.fg_dim },
+    MiniClueDescGroup             = { fg        = c.keyword },
+    MiniClueDescSingle            = { fg        = c.fg_main },
+    MiniClueNextKey               = { fg        = c.keybind, bold          = true },
+    MiniClueNextKeyWithPostkeys   = { fg        = c.err },
+    MiniClueSeparator             = { fg        = c.fg_dim },
     MiniClueTitle                 = { link      = "FloatTitle" },
 
     MiniCompletionActiveParameter = { underline = true },
 
     -- TODO: Improve these
-    MiniCursorword                = { bg        = c.bg_dim },
-    MiniCursorwordCurrent         = { bg        = c.bg_dim },
+    MiniCursorword                = { bg        = c.bg_alt },
+    MiniCursorwordCurrent         = { bg        = c.bg_alt },
 
     MiniDepsChangeAdded           = { link      = "diffAdded" },
     MiniDepsChangeRemoved         = { link      = "diffRemoved" },
@@ -48,31 +49,31 @@ function M.get(c, opts)
     MiniFilesFile                 = { fg        = c.fg_dim },
     MiniFilesNormal               = { link      = "NormalFloat" },
     MiniFilesTitle                = { link      = "FloatTitle" },
-    MiniFilesTitleFocused         = { fg        = c.border, bg             = c.bg_dim, bold      = true },
+    MiniFilesTitleFocused         = { fg        = c.border, bg             = c.bg_dim, bold         = true },
 
-    MiniHipatternsFixme           = { fg        = c.fg_term_black, bg      = c.err, bold         = true },
-    MiniHipatternsHack            = { fg        = c.fg_term_black, bg      = c.warning, bold     = true },
-    MiniHipatternsNote            = { fg        = c.fg_term_black, bg      = c.blue, bold        = true },
-    MiniHipatternsTodo            = { fg        = c.fg_term_black, bg      = c.info, bold        = true },
+    MiniHipatternsFixme           = { fg        = c.fg_term_black, bg      = c.err, bold            = true },
+    MiniHipatternsHack            = { fg        = c.fg_term_black, bg      = c.warning, bold        = true },
+    MiniHipatternsNote            = { fg        = c.fg_term_black, bg      = c.blue, bold           = true },
+    MiniHipatternsTodo            = { fg        = c.fg_term_black, bg      = c.info, bold           = true },
 
-    MiniIconsAzure                = { fg        = c.blue_cooler },
-    MiniIconsBlue                 = { fg        = c.blue },
-    MiniIconsCyan                 = { fg        = c.cyan_faint },
-    MiniIconsGreen                = { fg        = c.green },
-    MiniIconsGrey                 = { fg        = c.fg_dim },
-    MiniIconsOrange               = { fg        = c.yellow_warmer },
-    MiniIconsPurple               = { fg        = c.magenta_cooler },
-    MiniIconsRed                  = { fg        = c.red },
-    MiniIconsYellow               = { fg        = c.yellow },
+    MiniIconsAzure                = { fg        = c.blue_warmer },
+    MiniIconsBlue                 = { fg        = is_dark and c.blue_faint or c.blue_cooler },
+    MiniIconsCyan                 = { fg        = is_dark and c.cyan_faint or c.cyan },
+    MiniIconsGreen                = { fg        = is_dark and c.green_faint or c.green_warmer },
+    MiniIconsGrey                 = { fg        = is_dark and c.cyan_faint or "gray50" },
+    MiniIconsOrange               = { fg        = is_dark and c.red_faint or c.yellow_cooler },
+    MiniIconsPurple               = { fg        = is_dark and c.magenta_cooler or c.magenta_faint },
+    MiniIconsRed                  = { fg        = is_dark and c.red or c.red_faint },
+    MiniIconsYellow               = { fg        = is_dark and c.yellow_faint or c.yellow_warmer },
 
     MiniIndentscopePrefix         = { nocombine = true }, -- Make it invisible
     MiniIndentscopeSymbol         = { fg        = c.blue_warmer, nocombine = true },
 
     MiniJump                      = { bg        = c.magenta_warmer, fg     = "#ffffff" },
-    MiniJump2dDim                 = { link      = "Comment" },
-    MiniJump2dSpot                = { fg        = c.magenta_warmer, bold   = true, nocombine     = true },
-    MiniJump2dSpotAhead           = { fg        = c.info, bg               = c.bg_alt, nocombine = true },
-    MiniJump2dSpotUnique          = { fg        = c.accent_0, bold         = true, nocombine     = true, undercurl = true },
+    MiniJump2dDim                 = { bg        = c.bg_dim, fg             = c.fg_dim },
+    MiniJump2dSpot                = { fg        = c.fg_main, bg            = c.bg_char_0, bold      = true, nocombine = true },
+    MiniJump2dSpotAhead           = { fg        = c.fg_main, bg            = c.bg_char_1, nocombine = true },
+    MiniJump2dSpotUnique          = { fg        = c.fg_main, bg            = c.bg_char_2, bold      = true, undercurl = true },
 
     MiniMapNormal                 = { link      = "NormalFloat" },
     MiniMapSymbolCount            = { link      = "Special" },
@@ -87,7 +88,7 @@ function M.get(c, opts)
 
     MiniPickBorder                = { link      = "FloatBorder" },
     MiniPickBorderBusy            = { link      = "DiagnosticFloatingWarn" },
-    MiniPickBorderText            = { fg        = c.prompt, bg             = c.bg_dim, bold      = true },
+    MiniPickBorderText            = { fg        = c.prompt, bg             = c.bg_dim, bold         = true },
     MiniPickHeader                = { fg        = c.name, bold             = true },
     MiniPickIconDirectory         = { link      = "Directory" },
     MiniPickIconFile              = { link      = "MiniPickNormal" },
@@ -113,12 +114,12 @@ function M.get(c, opts)
     MiniStatuslineFileinfo        = { fg        = c.fg_mode_line, bg       = c.bg_mode_line },
     MiniStatuslineFilename        = { fg        = c.fg_mode_line, bg       = c.bg_dim },
     MiniStatuslineInactive        = { fg        = c.fg_mode_line, bg       = c.bg_mode_line },
-    MiniStatuslineModeCommand     = { fg        = c.fg_term_black, bg      = c.yellow, bold      = true },
-    MiniStatuslineModeInsert      = { fg        = c.fg_term_black, bg      = c.green, bold       = true },
-    MiniStatuslineModeNormal      = { fg        = c.fg_term_black, bg      = c.blue, bold        = true },
-    MiniStatuslineModeOther       = { fg        = c.fg_term_black, bg      = c.cyan, bold        = true },
-    MiniStatuslineModeReplace     = { fg        = c.fg_term_black, bg      = c.red, bold         = true },
-    MiniStatuslineModeVisual      = { fg        = c.fg_term_black, bg      = c.magenta, bold     = true },
+    MiniStatuslineModeCommand     = { fg        = c.fg_term_black, bg      = c.yellow, bold         = true },
+    MiniStatuslineModeInsert      = { fg        = c.fg_term_black, bg      = c.green, bold          = true },
+    MiniStatuslineModeNormal      = { fg        = c.fg_term_black, bg      = c.blue, bold           = true },
+    MiniStatuslineModeOther       = { fg        = c.fg_term_black, bg      = c.cyan, bold           = true },
+    MiniStatuslineModeReplace     = { fg        = c.fg_term_black, bg      = c.red, bold            = true },
+    MiniStatuslineModeVisual      = { fg        = c.fg_term_black, bg      = c.magenta, bold        = true },
 
     MiniSurround                  = { bg        = c.yellow_cooler, fg      = c.fg_term_black },
 
