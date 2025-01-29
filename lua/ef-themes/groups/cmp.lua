f61185dba2a6a38cd570561aec6be8937684d3a6
oonamo <onam75hernandez@gmail.com>
2025-01-28 17:07:42 -0800

    style: align groups

--- lua/ef-themes/groups/cmp.lua
+++ lua/ef-themes/groups/cmp.lua
@@ -3,21 +3,21 @@ local M = {}
 ---@param c Ef-Theme
 ---@param opts Ef-themes.Config
 function M.get(c, opts)
-  -- stylua:
+  -- stylua: ignore
   return {
-    CmpDocumentation = { fg = c.fg_main, bg = c.bg_dim },
-    CmpDocumentationBorder = { fg = c.border, bg = c.bg_dim },
-    CmpGhostText = { fg = c.fg_alt },
-    CmpItemAbbr = { fg = c.fg_main },
-    CmpItemAbbrDeprecated = { fg = c.fg_dim, bg = "NONE", strikethrough = true },
-    CmpItemAbbrMatch = { fg = c.accent_0, bg = "NONE", bold = true },
-    CmpItemAbbrMatchFuzzy = { fg = c.accent_0, bg = "NONE", bold = true },
-    CmpItemKindCodeium = { fg = c.cyan_faint, bg = "NONE" },
-    CmpItemKindCopilot = { fg = c.cyan_faint, bg = "NONE" },
-    CmpItemKindSupermaven = { fg = c.cyan_faint, bg = "NONE" },
-    CmpItemKindDefault = { fg = c.fg_dim, bg = "NONE" },
-    CmpItemKindTabNine = { fg = c.cyan_faint, bg = "NONE" },
-    CmpItemMenu = { fg = c.fg_main, bg = c.bg_inactive },
+    CmpDocumentation       = { fg = c.fg_main, bg    = c.bg_dim },
+    CmpDocumentationBorder = { fg = c.border, bg     = c.bg_dim },
+    CmpGhostText           = { fg = c.fg_alt },
+    CmpItemAbbr            = { fg = c.fg_main },
+    CmpItemAbbrDeprecated  = { fg = c.fg_dim, bg     = "NONE", strikethrough = true },
+    CmpItemAbbrMatch       = { fg = c.accent_0, bg   = "NONE", bold          = true },
+    CmpItemAbbrMatchFuzzy  = { fg = c.accent_0, bg   = "NONE", bold          = true },
+    CmpItemKindCodeium     = { fg = c.cyan_faint, bg = "NONE" },
+    CmpItemKindCopilot     = { fg = c.cyan_faint, bg = "NONE" },
+    CmpItemKindSupermaven  = { fg = c.cyan_faint, bg = "NONE" },
+    CmpItemKindDefault     = { fg = c.fg_dim, bg     = "NONE" },
+    CmpItemKindTabNine     = { fg = c.cyan_faint, bg = "NONE" },
+    CmpItemMenu            = { fg = c.fg_main, bg    = c.bg_inactive },
   }
 end
 
--- lua/ef-themes/groups/render_markdown.lua
+++ lua/ef-themes/groups/render_markdown.lua
@@ -3,12 +3,13 @@ local M = {}
 ---@param c Ef-Theme
 ---@param opts Ef-themes.Config
 function M.get(c, opts)
+  -- stylua: ignore
   return {
-    RenderMarkdownBullet = { fg = c.accent_0 }, -- horizontal rule
-    RenderMarkdownCode = { bg = c.bg_inactive },
-    RenderMarkdownDash = { fg = c.accent_1 }, -- horizontal rule
-    RenderMarkdownTableHead = { fg = c.prose_table, bold = true },
-    RenderMarkdownTableRow = { fg = c.prose_table },
+    RenderMarkdownBullet     = { fg = c.accent_0 }, -- horizontal rule
+    RenderMarkdownCode       = { bg = c.bg_inactive },
+    RenderMarkdownDash       = { fg = c.accent_1 }, -- horizontal rule
+    RenderMarkdownTableHead  = { fg = c.prose_table, bold = true },
+    RenderMarkdownTableRow   = { fg = c.prose_table },
     RenderMarkdownCodeInline = "@markup.raw.markdown_inline",
   }
 end
