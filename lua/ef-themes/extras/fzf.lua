13013d21e2ae641ecceb30beb117a25c40b945d6
oonamo <onam75hernandez@gmail.com>
2025-01-29 20:02:33 -0800

    fix(fzf): Fix shell escape + use actual corresponding highlights

--- lua/ef-themes/extras/fzf.lua
+++ lua/ef-themes/extras/fzf.lua
@@ -6,10 +6,11 @@ export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
 --highlight-line \
 --ansi \
 --layout=reverse \
---border=none
---color=bg+:${bg_hl_line},bg:${bg_dim},spinner:${magenta_cooler},hl:${blue_faint} \
+--border=none \
+--color=bg+:${bg_completion},bg:${bg_dim},spinner:${magenta_cooler},hl:${accent_0} \
 --color=fg:${fg_main},header:${name},info:${fg_dim},pointer:${magenta_cooler} \
---color=marker:${magenta_cooler},fg+:${bg_hl_line},prompt:${blue_faint},hl+:${blue_faint} \
+--color=marker:${magenta_cooler},fg+:${accent_0},prompt:${blue_faint},hl+:${accent_0} \
+
 
 # On powershell use do `nvim $PROFILE` and paste
 # $ENV:FZF_DEFAULT_OPTS =@"
@@ -17,10 +18,10 @@ export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
 # --ansi
 # --layout=reverse
 # --border=none
-# --color=bg+:${bg_hl_line},bg:${bg_dim},spinner:${magenta_cooler},hl:${blue_faint}
+# --color=bg+:${bg_completion},bg:${bg_dim},spinner:${magenta_cooler},hl:${accent_0}
 # --color=fg:${fg_main},header:${name},info:${fg_dim},pointer:${magenta_cooler}
-# --color=marker:${magenta_cooler},fg+:${bg_hl_line},prompt:${blue_faint},hl+:${blue_faint}
-# "@
+# --color=marker:${magenta_cooler},fg+:${accent_0},prompt:${blue_faint},hl+:${accent_0}
+#"@
   ]]
 end
 
