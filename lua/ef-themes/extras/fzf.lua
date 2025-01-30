a94c223c5198abf2d6f4f16bbd2279d8ce7de4c8
oonamo <onam75hernandez@gmail.com>
2025-01-29 20:27:37 -0800

    refactor(extra): try darker background on fzf

--- lua/ef-themes/extras/fzf.lua
+++ lua/ef-themes/extras/fzf.lua
@@ -7,7 +7,7 @@ export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
 --ansi \
 --layout=reverse \
 --border=none \
---color=bg+:${bg_completion},bg:${bg_dim},spinner:${magenta_cooler},hl:${accent_0} \
+--color=bg+:${bg_completion},bg:${bg_inactive},spinner:${magenta_cooler},hl:${accent_0} \
 --color=fg:${fg_main},header:${name},info:${fg_dim},pointer:${magenta_cooler} \
 --color=marker:${magenta_cooler},fg+:${fg_main},prompt:${blue_faint},hl+:${accent_0} \
 "
@@ -18,7 +18,7 @@ export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
 # --ansi
 # --layout=reverse
 # --border=none
-# --color=bg+:${bg_completion},bg:${bg_dim},spinner:${magenta_cooler},hl:${accent_0}
+# --color=bg+:${bg_completion},bg:${bg_inactive},spinner:${magenta_cooler},hl:${accent_0}
 # --color=fg:${fg_main},header:${name},info:${fg_dim},pointer:${magenta_cooler}
 # --color=marker:${magenta_cooler},fg+:${fg_main},prompt:${blue_faint},hl+:${accent_0}
 #"@
