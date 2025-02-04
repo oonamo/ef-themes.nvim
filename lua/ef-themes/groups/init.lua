88ec03f95e8d43f24421999c7b132c2528ace8d9
oonamo <onam75hernandez@gmail.com>
2025-02-04 14:53:04 -0800

    style(groups): Sort groups

--- lua/ef-themes/groups/init.lua
+++ lua/ef-themes/groups/init.lua
@@ -7,13 +7,13 @@ local M = {
     gitsigns =       {             label = "gitsigns.nvim",   url = "https://github.com/lewis6991/gitsigns.nvim",                   default = false },
     mini =           {             label = "mini.nvim",       url = "https://github.com/echasnovski/mini.nvim",                     default = true },
     neogit =         {             label = "Neogit",          url = "https://github.com/TimUntersberger/neogit",                    default = false },
+    orgmode =        {             label= "nvim-orgmode",     url = "https://github.com/nvim-orgmode/orgmode" },
     render_markdown ={             label= "render-markdown",  url = "https://github.com/MeanderingProgrammer/render-markdown.nvim", default = false },
     semantic_tokens ={             label= "semantic-tokens (native lsp)",                                                          default = false },
     snacks =         {             label = "snacks",          url = "https://github.com/folke/snacks.nvim",                         default = false },
     telescope =      {             label = "telescope",       url = "https://github.com/nvim-telescope/telescope.nvim" ,            default = false},
     treesitter =     {             label = "treesitter (native)",                                                                   default = true },
     which_key =      {             label = "which-key.nvim",  url = "https://github.com/folke/which-key.nvim",                      default = false },
-    orgmode =        {             label= "nvim-orgmode",     url = "https://github.com/nvim-orgmode/orgmode" }
   },
 }
 
